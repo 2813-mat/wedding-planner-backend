9a4177925dc1f899980b97216d63a2d0906c8547
Mateus Bispo <mateushas.advir@gmail.com>
2026-01-31 19:55:08 -0300

    fix: remove queries logs

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,13 +7,7 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
-    super({
-      // Opcional: log queries em dev
-      log:
-        process.env.NODE_ENV === 'development'
-          ? ['query', 'info', 'warn', 'error']
-          : ['error'],
-    });
+    super();
   }
 
   async onModuleInit() {
@@ -24,7 +18,6 @@ export class PrismaService
     await this.$disconnect();
   }
 
-  // Opcional: helper para clean shutdown em testes ou graceful shutdown
   async enableShutdownHooks(app: any) {
     process.on('SIGINT', async () => {
       await this.$disconnect();
